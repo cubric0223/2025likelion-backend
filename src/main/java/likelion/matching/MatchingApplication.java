7ab33f323d38723f611b92745ef4b72d56f61150
변중연 <byeonjung-yeon@byeonjung-yeon-ui-MacBookAir.local>
2025-05-02 13:27:05 +0900

    modified EntityScan at MatchingApplication.java

--- src/main/java/likelion/matching/MatchingApplication.java
+++ src/main/java/likelion/matching/MatchingApplication.java
@@ -2,8 +2,10 @@ package likelion.matching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "likelion.matching.domain.entity")
 public class MatchingApplication {
 
 	public static void main(String[] args) {
